0e316fa7c2ef7688fecef7f487f6a9bb44e0c86e
Shinoda <oriharu510@yahoo.co.jp>
2018-01-12 16:18:24 +0000

    create Load_action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/' => "home#top"
   get 'about' => "home#about"
   get 'Run_away_rat' => "home#Run_away_rat"
+  get 'load_action' => "home#load_action"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
